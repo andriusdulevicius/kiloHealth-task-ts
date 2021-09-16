9ac7e65fd9018251211fdd2477ae11fb57cc537c
AndriusDulevicius <andriusdulevicius@email.com>
2021-09-16 19:21:32 +0300

    some minor changes

--- src/components/wrappers/Container.js
+++ src/components/wrappers/Container.js
@@ -13,14 +13,3 @@ export const ContainerSmall = styled(Container)`
   padding: ${(props) => props.padding || '0 1rem'};
   max-width: 48rem;
 `;
-
-export const FlexWrapper = styled.div`
-  display: ${(props) => props.display || 'flex'};
-  justify-content: ${(props) => props.justifyContent || 'center'};
-  align-items: ${(props) => props.alignItems || 'center'};
-  gap: ${(props) => props.gap || '1rem'};
-  flex-direction: ${(props) => props.flexDirection || 'row'};
-  flex-wrap: ${(props) => props.flexWrap || 'wrap'};
-  margin: ${(props) => props.margin || 'auto'};
-  padding: ${(props) => props.padding || '0 1rem'};
-`;
